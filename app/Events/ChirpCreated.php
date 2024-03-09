78271f386c2f3a068922f7d71311150c43370d85
Bamidele Sunday <sunday.bamidele@gmail.com>
2024-03-09 20:20:40 +0000

    Work in progress

--- app/Events/ChirpCreated.php
+++ app/Events/ChirpCreated.php
@@ -2,6 +2,8 @@
 
 namespace App\Events;
 
+
+use App\Models\Chirp;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
@@ -17,7 +19,7 @@ class ChirpCreated
     /**
      * Create a new event instance.
      */
-    public function __construct()
+    public function __construct(public Chirp $chirp)
     {
         //
     }
---
+++ app/Listeners/SendChirpCreatedNotifications.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ChirpCreated;
+use App\Models\User;
+use App\Notifications\NewChirp;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class SendChirpCreatedNotifications
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(ChirpCreated $event): void
+    {
+        //
+    }
+}
